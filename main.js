76152ed1da8f8ccba6bb0e1a522c6c09ae29d6d9
nicholas-kaloger <njk.kaloger@gmail.com>
2018-09-04 14:44:54 +1000

    Changed Menu

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow() {
   win = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  win.loadFile("src/index.html");
+  win.loadFile("src/components/home/index.html");
 
   // Open the DevTools.
   //win.webContents.openDevTools();
@@ -25,39 +25,148 @@ function createWindow() {
 
   var menu = Menu.buildFromTemplate([
     {
-      label: "Menu",
+      label: "Edit",
       submenu: [
-        { label: "Adjust Notification Value" },
         {
-          label: "CoinMarketCap",
-          click() {
-            shell.openExternal("http://coinmarketcap.com");
+          label: "Undo",
+          accelerator: "CmdOrCtrl+Z",
+          role: "undo"
+        },
+        {
+          label: "Redo",
+          accelerator: "Shift+CmdOrCtrl+Z",
+          role: "redo"
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Cut",
+          accelerator: "CmdOrCtrl+X",
+          role: "cut"
+        },
+        {
+          label: "Copy",
+          accelerator: "CmdOrCtrl+C",
+          role: "copy"
+        },
+        {
+          label: "Paste",
+          accelerator: "CmdOrCtrl+V",
+          role: "paste"
+        },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          role: "selectall"
+        }
+      ]
+    },
+    {
+      label: "View",
+      submenu: [
+        {
+          label: "Reload",
+          accelerator: "CmdOrCtrl+R",
+          click: (item, focusedWindow) => {
+            if (focusedWindow) {
+              // on reload, start fresh and close any old
+              // open secondary windows
+              if (focusedWindow.id === 1) {
+                BrowserWindow.getAllWindows().forEach(win => {
+                  if (win.id > 1) win.close();
+                });
+              }
+              focusedWindow.reload();
+            }
           }
         },
-        { type: "separator" },
         {
-          label: "Exit",
-          click() {
-            app.quit();
+          label: "Toggle Full Screen",
+          accelerator: (() => {
+            if (process.platform === "darwin") {
+              return "Ctrl+Command+F";
+            } else {
+              return "F11";
+            }
+          })(),
+          click: (item, focusedWindow) => {
+            if (focusedWindow) {
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+            }
+          }
+        },
+        {
+          label: "Toggle Developer Tools",
+          accelerator: (() => {
+            if (process.platform === "darwin") {
+              return "Alt+Command+I";
+            } else {
+              return "Ctrl+Shift+I";
+            }
+          })(),
+          click: (item, focusedWindow) => {
+            if (focusedWindow) {
+              focusedWindow.toggleDevTools();
+            }
+          }
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "App Menu Demo",
+          click: function(item, focusedWindow) {
+            if (focusedWindow) {
+              const options = {
+                type: "info",
+                title: "Application Menu Demo",
+                buttons: ["Ok"],
+                message:
+                  "This demo is for the Menu section, showing how to create a clickable menu item in the application menu."
+              };
+              dialog.showMessageBox(focusedWindow, options, function() {});
+            }
           }
         }
       ]
     },
     {
-      label: "Info",
+      label: "Window",
+      role: "window",
       submenu: [
-        { label: "Adjust Notification Value" },
         {
-          label: "CoinMarketCap",
-          click() {
-            shell.openExternal("http://coinmarketcap.com");
-          }
+          label: "Minimize",
+          accelerator: "CmdOrCtrl+M",
+          role: "minimize"
         },
-        { type: "separator" },
         {
-          label: "Exit",
-          click() {
-            app.quit();
+          label: "Close",
+          accelerator: "CmdOrCtrl+W",
+          role: "close"
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Reopen Window",
+          accelerator: "CmdOrCtrl+Shift+T",
+          enabled: false,
+          key: "reopenMenuItem",
+          click: () => {
+            app.emit("activate");
+          }
+        }
+      ]
+    },
+    {
+      label: "Help",
+      role: "help",
+      submenu: [
+        {
+          label: "Learn More",
+          click: () => {
+            shell.openExternal("http://electron.atom.io");
           }
         }
       ]
